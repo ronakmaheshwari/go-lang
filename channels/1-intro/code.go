13c10b114254a3350c6e0d31cd0c91a2351f3d41
ronakmaheshwari <ronakmaheshwari077@gmail.com>
2026-03-24 09:56:26 +0530

    Channels finishing

---
+++ channels/1-intro/code.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sendEmail(message string) {
+	go func ()  {
+		time.Sleep(time.Millisecond * 250)
+		fmt.Printf("Email received: '%s'\n", message)
+	}()
+	fmt.Printf("Email sent: '%s'\n", message)
+}
+
+func test(message string) {
+	sendEmail(message)
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("========================")
+}
+
+func main() {
+	test("Hello there Stacy!")
+	test("Hi there John!")
+	test("Hey there Jane!")
+}
---
+++ pointers/4-pointer-receiver/code.go
@@ -0,0 +1,44 @@
+package main
+import "fmt"
+
+type email struct {
+	fromAddress string
+	toAddress string
+	message string
+}
+
+func (e *email) setMessage(newMsg string) {
+	e.message = newMsg
+}
+
+func test(e *email, newMessage string) {
+	fmt.Println("-- before --")
+	e.print()
+	fmt.Println("-- end before --")
+	e.setMessage("this is my second draft")
+	fmt.Println("-- after --")
+	e.print()
+	fmt.Println("-- end after --")
+	fmt.Println("==========================")
+}
+
+func (e email) print() {
+	fmt.Println("message:", e.message)
+	fmt.Println("fromAddress:", e.fromAddress)
+	fmt.Println("toAddress:", e.toAddress)
+
+}
+
+func main() {
+	test(&email{
+		message:     "this is my first draft",
+		fromAddress: "sandra@mailio-test.com",
+		toAddress:   "bullock@mailio-test.com",
+	}, "this is my second draft")
+
+	test(&email{
+		message:     "this is my third draft",
+		fromAddress: "sandra@mailio-test.com",
+		toAddress:   "bullock@mailio-test.com",
+	}, "this is my fourth draft")
+}
